664128169c942fc21e4918e78c3b2daccbf5bebd
jkracz <joseph.kracz@gmail.com>
2022-09-17 17:20:16 -0400

    Created a particles component to make the background interactive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import React from 'react';
+import { React } from 'react';
+
 import Navigation from './compnents/Navigation/Navigation';
 import Logo from './compnents/Logo/Logo';
 import ImageLinkForm from './compnents/ImageLinkForm/ImageLinkForm';
 import Rank from './compnents/Rank/Rank';
+// import Particle from './compnents/Particle/Particle';
 
 function App() {
   return (
     <div className="App">
+      {/* <Particle /> */}
       <Navigation />
       <Logo />
       <Rank />
