ea942ff1f06baa119a6c62eb1d9f4927c6022e70
jkracz <joseph.kracz@gmail.com>
2022-09-20 22:44:18 -0400

    started working on adding the facial recognition model

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import './App.css';
-import { React } from 'react';
+import { Component, React } from 'react';
 
 import Navigation from './compnents/Navigation/Navigation';
 import Logo from './compnents/Logo/Logo';
 import ImageLinkForm from './compnents/ImageLinkForm/ImageLinkForm';
 import Rank from './compnents/Rank/Rank';
-// import Particle from './compnents/Particle/Particle';
+import Particle from './compnents/Particle/Particle';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Particle /> */}
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognizer /> */}
-    </div>
-  );
+import Clarifai from 'clarifai'
+
+const MODEL_VERSION_ID = 'a403429f2ddf4b49b307e318f00e528b';    
+const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+const app = new Clarifai.App({
+  apiKey: "08e81ce8f9e34023b240c6b7595bf067"
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models.predict(MODEL_VERSION_ID, IMAGE_URL).then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particle />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognizer /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
