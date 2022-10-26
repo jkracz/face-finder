19453bf0162d1151d79555872d45bd193f0107d7
jkracz <joseph.kracz@gmail.com>
2022-10-25 18:37:00 -0700

    updated the particles package for the background

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Navigation from './compnents/Navigation/Navigation';
 import Logo from './compnents/Logo/Logo';
 import ImageLinkForm from './compnents/ImageLinkForm/ImageLinkForm';
 import Rank from './compnents/Rank/Rank';
-import Particle from './compnents/Particle/Particle';
+import FaceRecognizer from './compnents/FaceRecognizer/FaceRecognizer';
 
+import ParticlesBg from 'particles-bg'
 import Clarifai from 'clarifai'
 
-const MODEL_VERSION_ID = 'a403429f2ddf4b49b307e318f00e528b';    
 const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
 
 const app = new Clarifai.App({
@@ -21,18 +21,19 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
     }
   }
 
   onInputChange = (event) => {
-    console.log(event);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log("click");
-    app.models.predict(MODEL_VERSION_ID, IMAGE_URL).then(
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function(response) {
-        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
       function(err) {
         console.log(err);
@@ -43,12 +44,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particle />
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        {/* <FaceRecognizer /> */}
+        <FaceRecognizer imageUrl={this.state.imageUrl} />
+        <ParticlesBg type="square" bg={true} />
       </div>
     );
   }
