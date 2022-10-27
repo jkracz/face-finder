24eee5954ba2eaaa147b8962aeb6e23f3117bbdb
jkracz <joseph.kracz@gmail.com>
2022-10-26 17:56:03 -0700

    added single face box logic

--- src/components/FaceRecognizer/FaceRecognizer.js
+++ src/components/FaceRecognizer/FaceRecognizer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./FaceRecognizer.css";
 
-const FaceRecognizer = ({ imageUrl }) => {
+const FaceRecognizer = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img alt="" src={imageUrl} width="500px" height="auto" />
+        <img id="inputimage" alt="" src={imageUrl} width="500px" height="auto" />
+        <div className="bounding-box" style={{left: box.leftCol, right: box.rightCol, top: box.topRow, bottom: box.bottomRow}}></div>
       </div>
     </div>
   );
